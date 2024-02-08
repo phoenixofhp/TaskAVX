2f3c02cf1658810e7616e13bded7299605f70146
phoenixofhp <2minesweeper2@gmail.com>
2024-02-08 23:58:21 +0300

    Revert "Actual"
    This reverts commit 0987b1a3482f7ae90c3b6bc3c5adb9658c1b65a5.

--- TaskAVX/Source.cpp
+++ TaskAVX/Source.cpp
@@ -1,25 +1,22 @@
 #include <iostream>
-#include <iomanip> //for std::setprecision
-#include <immintrin.h> //SIMD instructions
 #include <chrono>
+#include <immintrin.h>
 
-uint64_t NonOptFuncMults = 0;
-uint64_t OptFuncLoopMults = 0;
+uint32_t NonOptFuncLoopExec = 0;
+uint32_t OptFuncLoopExec = 0;
 
 static void firNonOptimized(float* x, float* y, float* b, size_t n) {
-	if (n == 0) return;
 	std::reverse(b, b + n);
 	for (size_t i = 0; i < n; i++) {
 		for (size_t j = 0; j <= i; j++) {
 			y[i] += b[j] * x[j];
-			NonOptFuncMults++;
+			NonOptFuncLoopExec++;
 		}
 	}
 	std::reverse(b, b + n);
 }
 
 static void firOptimized(float* x, float* y, float* b, size_t n) {
-	if (n == 0) return;
 	std::reverse(b, b + n);
 	for (size_t i = 0; i < n; i++) {
 		size_t avx256_group_operations = (i + 1) / 8; //avx256 holds 8 floats
@@ -34,11 +31,11 @@ static void firOptimized(float* x, float* y, float* b, size_t n) {
 			big_group_mult = _mm256_mul_ps(big_group_x, big_group_h);
 			float* big_group_result = (float*)&big_group_mult;
 			for (size_t k = 0; k < 8; k++) y[i] += big_group_result[k];
-			OptFuncLoopMults++;
+			OptFuncLoopExec++;
 		}
 		for (size_t j = (avx256_group_operations * 8); j < i + 1; j++) {
 			y[i] += x[j] * b[j];
-			OptFuncLoopMults++;
+			OptFuncLoopExec++;
 		}
 	}
 	std::reverse(b, b + n);
@@ -53,22 +50,20 @@ int main() {
 
 	t1 = std::chrono::high_resolution_clock::now();
 	t2 = std::chrono::high_resolution_clock::now();
-	t_diff = std::chrono::duration<double, std::micro>(t1 - t2).count(); //Compensation
+	t_diff = std::chrono::duration<double, std::milli>(t1 - t2).count(); //overhead compensation
 
-	std::cout << "Length\t\
-Default, mcs\t\
-AVX, mcs\t\
-Default Mults\t\
-AVX Mults\t\
-Advantage, %\n";
+	std::cout << "Length\tNon optimized\tOptimized\tNon Opt\t\tOpt\t\tIs same\n";
 
-	for (size_t i = 1; i <= std::pow(2,20); i *= 2) {
+	for (size_t i = 1; i <= 65536; i *= 2) {
 
 		float* x = new float[i];
 		float* y = new float[i];
 		float* b = new float[i];
 		float* y_comp = new float[i];
-
+		/*
+		memset(x, 0.3333, i);
+		memset(y, 0.0, i);
+		memset(b, 0.6666, i);*/
 		std::fill(x, x + i, 0.3333);
 		std::fill(y, y + i, 0.0);
 		std::fill(b, b + i, 0.6666);
@@ -76,7 +71,7 @@ Advantage, %\n";
 		t1 = std::chrono::high_resolution_clock::now();
 		firNonOptimized(x, y, b, i);
 		t2 = std::chrono::high_resolution_clock::now();
-		elapsed_time_non_opt = std::chrono::duration<double, std::micro>(t2 - t1).count() - t_diff;
+		elapsed_time_non_opt = std::chrono::duration<double, std::milli>(t2 - t1).count() - t_diff;
 
 		std::copy(y, y + i, y_comp);
 		std::fill(y, y + i, 0.0);
@@ -84,15 +79,10 @@ Advantage, %\n";
 		t1 = std::chrono::high_resolution_clock::now();
 		firOptimized(x, y, b, i);
 		t2 = std::chrono::high_resolution_clock::now();
-		elapsed_time_opt = std::chrono::duration<double, std::micro>(t2 - t1).count() - t_diff;
+		elapsed_time_opt = std::chrono::duration<double, std::milli>(t2 - t1).count() - t_diff;
 
-		std::cout << std::setprecision(7) \
-			<< i << '\t' \
-			<< elapsed_time_non_opt << "\t\t" \
-			<< elapsed_time_opt<< "\t\t" \
-			<< NonOptFuncMults << "\t\t" \
-			<< OptFuncLoopMults << "\t" \
-			<< elapsed_time_non_opt / elapsed_time_opt * 100.0 << "%" << '\n';
+		std::cout << i << '\t' << elapsed_time_non_opt << "\t\t" << elapsed_time_opt\
+			<< "\t\t" << NonOptFuncLoopExec << "\t\t" << OptFuncLoopExec << "\t\t" << std::equal(y, y + i, y_comp) << '\n';
 
 		delete[] x;
 		delete[] y;
